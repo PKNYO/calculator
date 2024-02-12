9f1b47fb4363fe3f7fa405486a3e2531c6cc9406
PKNYO <147757429+PKNYO@users.noreply.github.com>
2024-02-12 23:47:27 +0100

    Create function to populate screen and manage vars
    There are new function able to populate the
    numbers and manage the variables to operate
    when equals pressed.

--- main.js
+++ main.js
@@ -3,13 +3,16 @@ const substract = function(a, b) {return a - b};
 const multiply = function(a, b) {return a * b};
 const divide = function(a, b) {return a / b};
 
-const buttons = document.querySelectorAll(".button")
-const screen = document.querySelector(" screen")
+const digitButtons = document.querySelectorAll(".digit");
+const operatorButtons = document.querySelectorAll(".operator");
+const equalsButton = document.querySelector(".equals");
+const screen = document.querySelector(".screen");
 
-let firstNumber = "";
+let firstNumber = 0;
 let secondNumber = "";
 let operator = "";
-let screenText = "";
+let operatorIsSelected = false;
+let operatorKeySelected = ""
 
 function operate(a, b, operator) {
     switch (operator) {
@@ -25,3 +28,46 @@ function operate(a, b, operator) {
             break;
     }
 }
+
+function populateScreen(event) {
+    const key = event.target;
+
+    if (screen.textContent == 0) {
+        screen.textContent = "";
+    }
+
+    if (operatorIsSelected == false) {
+        screen.textContent += key.textContent;
+    } 
+    if (operatorIsSelected == true) {
+        operatorKeySelected.classList.remove("selected");
+        operatorIsSelected = false;
+        screen.textContent = "";
+        screen.textContent += key.textContent;
+    }
+}
+
+function operatorSelected(event) {
+    const key = event.target;
+
+    operatorKeySelected = key
+    firstNumber = +screen.textContent;
+    operator = key.id;
+    key.classList.add("selected");
+    operatorIsSelected = true;
+}
+
+function equalSelected(event) {
+    secondNumber = screen.textContent;
+    screen.textContent = operate(+firstNumber, +secondNumber, operator)
+}
+
+digitButtons.forEach((button) => {
+    button.addEventListener("click", (e) => populateScreen(e))
+})
+
+operatorButtons.forEach((button) => {
+    button.addEventListener("click", (e) => operatorSelected(e))
+})
+
+equalsButton.addEventListener("click", (e) => equalSelected(e))
